a61b233750a1fd79a24b8868dfb5b5fd49e8874f
Victor Silva <Victor Silva>
2016-10-07 13:01:46 -0300

    Add new file!

---
+++ Set-SCCMExcludeServer.ps1
@@ -0,0 +1,16 @@
+﻿<#
+ 	.SYNOPSIS
+        Función para excluir servidores del scope de SCCM a la hora de realizar la instalación de los agentes.
+    .DESCRIPTION
+        <Función para excluir servidores del scope de SCCM a la hora de realizar la instalación de los agentes.
+    .PARAMETER  Server
+        Indicar el o los servidores que van a ser excluidos de la implementación de los agentes.
+    .EXAMPLE
+        Set-SCCMExcludeServer.ps1 -Server $Server
+        
+#>
+
+Param
+(
+
+)
