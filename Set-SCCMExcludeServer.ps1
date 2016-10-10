b7c2b6e5c57bab2127bcb0e03eed7108ffa2ccd8
Victor Silva <vmsilvamolina@gmail.com>
2016-10-10 11:02:24 -0300

    Update Set-SCCMExcludeServer.ps1

--- Set-SCCMExcludeServer.ps1
+++ Set-SCCMExcludeServer.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
  	.SYNOPSIS
         Función para excluir servidores del scope de SCCM a la hora de realizar la instalación de los agentes.
     .DESCRIPTION
@@ -12,5 +12,11 @@
 
 Param
 (
-
+    [Parameter(Mandatory=$true)][String] $Server
 )
+
+$RegistryPath = "HKEY_LOCAL_MACHINE/Software/Microsoft/SMS/Components/SMS_DISCOVERY_DATA_MANAGER"
+$Name = "ExcludeServers"
+
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Server -PropertyType DWORD -Force | Out-Null
+Write-Verbose "Server: $Server excluded!"
