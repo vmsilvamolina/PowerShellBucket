bc002b774fa3266acb087ffe7206841769792191
Victor Silva <vmsilvamolina@gmail.com>
2016-10-28 13:47:00 -0300

    RemoteApp

---
+++ RemoteApp.ps1
@@ -0,0 +1,51 @@
+﻿### RemoteApp - Casinos del estado ###
+cls
+Import-Module RemoteDesktop
+Import-Module ActiveDirectory
+$Sucursales = Import-Csv C:\Users\sorrego\Desktop\Sucursales_all.csv -Delimiter ";"
+Write-host "RemoteApp" -ForegroundColor Blue
+foreach ($Sucursal in $Sucursales) {
+    #Obtengo el número correspondiente
+    $numeroSucursal = $Sucursal.Numero
+    $nombreSucursal = $Sucursal.Nombre
+    #Imprimo (sólo por información) la sucursal en la que voy a trabajar
+    Write-host "Sucursal: " $nombreSucursal $numeroSucursal -ForegroundColor Green
+    #Genero las RemoteApp para cada ejecutable
+    Write-host "    CliConta Apps" -ForegroundColor Green
+    #CliConta
+    [array]$exesCliConta = "app1","app2"
+    [string]$grupoFiltroConta = "*"+$numeroSucursal+"CliConta"
+    $GrupoCONTA = Get-ADGroup -filter {name -like $grupoFiltroConta}
+    foreach ($exeCliConta in $exesCliConta) {
+        if (Test-Path "E:\Sucursales\$numeroSucursal\CliConta\$exeCliConta.exe") {
+            try {
+                New-RDRemoteApp -Alias "$exeCliConta-$nombreSucursal" -DisplayName "$exeCliConta - $nombreSucursal" -FilePath "E:\Sucursales\$numeroSucursal\CliConta\$exeCliConta.exe" -ShowInWebAccess 1 -CollectionName "App Collection" -ConnectionBroker "remoteappserver.dgc.local" -UserGroups $GrupoCONTA.Name >> C:\users\sorrego\Desktop\Salida.txt
+                Write-host "        $exeCliConta - $numeroSucursal agregada correctamente"
+            } catch {
+            Write-Error $Error[0]
+            }
+        } else {
+            Write-host "        No existe el ejecutable $exeCliConta"
+        }
+    }
+    Write-host "    CliMaq Apps" -ForegroundColor Green
+    #CliMaq
+    [array]$exesCliMaq = "app3", "app4"
+    [string]$grupoFiltroMaq = "*"+$numeroSucursal+"CliMaq"
+    $GrupoMAQ = Get-ADGroup -filter {name -like $grupoFiltroMaq}
+    foreach ($exeCliMaq in $exesCliMaq) {
+        if (Test-Path "E:\Sucursales\$numeroSucursal\CliMaq\$exeCliMaq.exe") {
+            try {
+                New-RDRemoteApp -Alias "$exeCliMaq-$numeroSucursal" -DisplayName "$exeCliMaq - $nombreSucursal" -FilePath "E:\Sucursales\$numeroSucursal\CliMaq\$exeCliMaq.exe" -ShowInWebAccess 1 -CollectionName "App Collection" -ConnectionBroker "remoteappserver.dgc.local" -UserGroups $GrupoMAQ.Name >> C:\users\sorrego\Desktop\Salida.txt
+                Write-host "$exeCliMaq - $numeroSucursal agregada correctamente"
+            } catch {
+                Write-Error $Error[0]
+            }
+        } else {
+            Write-host "        No existe el ejecutable $exeCliMaq"
+        }
+    }
+    Write-host "    Grupo" -ForegroundColor Green
+    Write-Host "        CliConta: $GrupoCONTA"
+    Write-Host "        CliMaq: $GrupoMAQ"
+}
---
+++ ShareFolder-Casinos.ps1
@@ -0,0 +1,27 @@
+﻿### Share Folder - Casinos del estado ###
+
+$Sucursales = Import-Csv C:\Users\sorrego\Desktop\Sucursales.csv -Delimiter ";"
+Write-host "Share Folder" -ForegroundColor Blue
+foreach ($Sucursal in $Sucursales) {
+    #Obtengo el número correspondiente
+    $numeroSucursal = $Sucursal.Numero
+    $nombreSucursal = $Sucursal.Nombre
+    #Imprimo (sólo por información) la sucursal en la que voy a trabajar
+    Write-host "Sucursal: " $nombreSucursal $numeroSucursal -ForegroundColor Green
+    #Creo el Share CliConta
+    [string]$shareCliConta = $numeroSucursal+"CliConta”
+    try {
+    New-SMBShare –Name $shareCliConta –Path "E:\Sucursales\$numeroSucursal\CliConta" –FullAccess everyone >> C:\users\sorrego\Desktop\salida.txt
+    } catch {
+    Write-Error $Error[0]
+    }
+    Write-Host "    $shareCliConta creado correctamente"
+    #CliMaq
+    [string]$shareCliMaq = $numeroSucursal+"CliMaq”
+    try {
+    New-SMBShare –Name $shareCliMaq –Path “E:\Sucursales\$numeroSucursal\CliMaq” –FullAccess everyone >> C:\users\sorrego\Desktop\salida.txt
+    } catch {
+    Write-Error $Error[0]
+    }
+    Write-Host "    $shareCliMaq creado correctamente"
+}
